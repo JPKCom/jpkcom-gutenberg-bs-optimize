e219ab4b66ced7394b05b82c59da458f4446bc54
Jean Pierre Kolb <jp.kolb@creationell.de>
2025-02-19 09:03:52 +0100

    v1.0.2

--- jpkcom-gutenberg-bs-optimize.php
+++ jpkcom-gutenberg-bs-optimize.php
@@ -3,7 +3,7 @@
 Plugin Name: JPKCom Gutenberg Bootstrap Optimizer
 Plugin URI: https://www.jpkc.com/
 Description: Fixes and optimizes settings for Gutenberg and Bootstrap.
-Version: 1.0.2
+Version: 1.0.3
 Author: Jean Pierre Kolb <jpk@jpkc.com>
 Author URI: https://www.jpkc.com
 Requires at least: 6.7
@@ -14,15 +14,25 @@ GitHub Plugin URI: JPKCom/jpkcom-gutenberg-bs-optimize
 Primary Branch: main
 */
 
-function jpkcom_bs_deregister_plugin_admin_styles(): void {
+if ( ! defined( constant_name: 'WPINC' ) ) {
+	die;
+}
 
-  // Check if the specific plugin's stylesheet is enqueued in the admin area
-  if ( wp_style_is( 'areoi-bootstrap', 'enqueued' ) ) {
+if ( ! function_exists( function: 'jpkcom_bs_deregister_plugin_admin_styles' ) ) {
 
-      // Deregister the plugin's stylesheet in the admin area
-      wp_deregister_style( 'areoi-bootstrap' );
+
+  function jpkcom_bs_deregister_plugin_admin_styles(): void {
+
+    // Check if the specific plugin's stylesheet is enqueued in the admin area
+    if ( wp_style_is( 'areoi-bootstrap', 'enqueued' ) ) {
+
+        // Deregister the plugin's stylesheet in the admin area
+        wp_deregister_style( 'areoi-bootstrap' );
+
+    }
 
   }
+
 }
 
 add_action( 'admin_enqueue_scripts', 'jpkcom_bs_deregister_plugin_admin_styles', 100 );
